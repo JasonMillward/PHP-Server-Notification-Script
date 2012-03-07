8b413c407684d92cadcb4e4c934a7d12ac7ed057
Jason <jason@jcode.me>
2012-03-07 21:42:31 +1030

    Rearrange skeletons

--- main.php
+++ main.php
@@ -4,17 +4,18 @@
 	require_once original_functions.php
 	require_once custom_functions.php
 	
-	
+	// Things that only happen on the first of the month	
 	if ( todayIsTheFirst() ) {
-		$uptime = getUptime();
-		notify($uptime.SIGNATURE,false);
+		checkUptime();
+		readNetwork();	
 	}
+	
+	// Things that are being checked every x minutes
 
 	// Generic
 	checkLoad();
 	checkErrorLogs();
 	checkDisks();
-	readNetwork();
 
 	
 	// Custom
