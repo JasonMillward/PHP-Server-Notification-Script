2097fabe29c74df623d8b82dd4e629b219e8f825
Jason <jason@jcode.me>
2012-03-07 21:35:30 +1030

    More skeletons

--- main.php
+++ main.php
@@ -1,5 +1,10 @@
 <?php
-
+	// Time to include our files.
+	require_once config.php
+	require_once original_functions.php
+	require_once custom_functions.php
+	
+	
 	if ( todayIsTheFirst() ) {
 		$uptime = getUptime();
 		notify($uptime.SIGNATURE,false);
