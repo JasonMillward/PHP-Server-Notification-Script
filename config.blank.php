31d9e5c421866b1079e694fe662afe4231340525
Jason <jason@jcode.me>
2012-03-08 13:14:20 +1030

    Time to ignore some files, blank config made

---
+++ config.blank.php
@@ -0,0 +1,17 @@
+<?php
+// Define vars
+define( 'CONKEY',    '' );
+define( 'CONSECRET', '' );
+define( 'OATOKEN',   '' );
+define( 'OASECRET',  '' );
+define( 'DATE',      date( "F j, Y, g:i a" ) );
+
+define( 'DB_DSN', 'mysql:dbname=FILL_ME_IN;host=;port=;' );
+define( 'DB_USER', '' );
+define( 'DB_PASS', '' );
+define( 'DB_HOST', 'localhost' );
+
+$config['errorLogs'][] = array(
+    "path"        => "/",
+    "twitterName" => ""
+);
